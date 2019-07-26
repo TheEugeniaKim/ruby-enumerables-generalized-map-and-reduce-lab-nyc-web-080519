ea5d32473eb9c9e0292ea3a3be6a7aef78ae27fc
Eugenia Kim <elk303@nyu.edu>
2019-07-26 02:17:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,3 +7,18 @@ def map(array)
   end
   new
 end
+
+def reduce(array, starting_value = nil)
+  if starting_value != nil 
+     block = starting_value
+      i = 0
+   else 
+     block = array[0]
+     i = 1 
+   end 
+  while i < array.length 
+      block = (yield(block, array[i]))
+    i += 1
+end 
+   return block 
+ end  
